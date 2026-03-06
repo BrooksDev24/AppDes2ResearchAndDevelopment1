5ee4f7de7083505026d4f2e46bb8ec6228d6ff53
Brooks <BrooksDev24@gmail.com>
2026-03-06 16:22:39 -0600

    Uploading my GadgetHub SourceCode

---
+++ GadgetHub/GadgetHub.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GadgetHub.Domain.Abstract;
+using GadgetHub.WebUI.Models;
+
+namespace GadgetHub.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository myRepository;
+
+        public ProductController (IProductRepository productRepository)
+        {
+            this.myRepository = productRepository;
+        }
+
+        public int pageSize = 4;
+        public ViewResult List(int page = 1)
+        {
+            ProductsListViewModels model = new ProductsListViewModels
+            {
+                Products = myRepository.Products.OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * pageSize)
+                    .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = myRepository.Products.Count()
+                }
+            };
+            return View(model);
+
+        }
+    }
+}
---
+++ GadgetHub/GadgetHub.WebUI/Models/ProductsListViewModels.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using GadgetHub.Domain.Entities;
+
+namespace GadgetHub.WebUI.Models
+{
+    public class ProductsListViewModels
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
